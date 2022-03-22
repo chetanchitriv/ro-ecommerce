277f977bb08edcd40f8011d2179c7239c2b29ee3
chetanchitriv <chetanvinkal@gmail.com>
2022-03-22 17:52:47 +0530

    login design complet

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './my-account/login/login.component';
+import { SignupComponent } from './my-account/signup/signup.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+},
+{ path: "login", component: LoginComponent },
+{ path: "signup", component: SignupComponent },
+
+{path:"home",component:HomeComponent,}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
